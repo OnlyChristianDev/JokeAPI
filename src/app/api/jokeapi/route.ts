e2e2680f8d49f3a06591751b15757656ce53a323
christian <christiandeveloper123@gmail.com>
2024-11-17 11:27:59 -0300

    correção dos erros referente ao tratamento dos dados

--- src/app/api/jokeapi/route.ts
+++ src/app/api/jokeapi/route.ts
@@ -2,52 +2,37 @@ const jokes = [
     "Por que os químicos são ótimos em resolver problemas? Porque eles têm todas as soluções!",
     "Por que o desenvolvedor faliu? Porque ele usou todo o seu cache.",
     "Você já ouviu falar do cara que roubou o calendário? Ele pegou 12 meses!",
-    "O que ganha o melhor dentista do mundo? Uma pequena placa.",
     "Meus professores me disseram que eu nunca seria muito porque procrastino muito. Eu disse a eles: 'Esperem para ver!'",
-    "Minha memória ficou tão ruim que realmente me fez perder o emprego. Ainda estou empregado. Só não consigo lembrar onde.",
     "Quando em uma candidatura a emprego perguntam quem deve ser notificado em caso de emergência, sempre escrevo: 'Um médico muito bom'.",
     "Por que o médico está sempre calmo? Porque ele tem muitos pacientes.",
     "Por que o livro de matemática parece tão triste? Por causa de todos os seus problemas.",
-    "Qual é a comida favorita de um lobisomem? Lobisomens não são reais.",
     "Como chamar um cão mágico? Um Labracadabrador.",
     "Por que os pássaros voam para climas mais quentes no inverno? É muito mais fácil do que caminhar!",
     "Seja como um próton. Sempre seja positivo.",
     "Que tipo de carro o Yoda dirige? Toyoda.", 
     "O que o zero diz a um oito? Cinto legal.",
-    "Quantos profissionais de marketing são necessários para arrumar uma lâmpada? Nenhum, eles já automatizaram o processo.",
-    "Quer ouvir uma piada sobre o potássio? K.",
+    "O que o HTML disse ao CSS? “Eu gosto do seu estilo!.",
     "O que você faz quando suas piadas sobre ciência não tem graça? Continue tentando até conseguir uma reação.",
     "Por que o dinossauro atravessou a rua? Porque as galinhas ainda não existiam!",
-    "O que um pato subatômico diz? Quark.",
     "O que o Oceano Atlântico disse ao Oceano Índico? 'Experimente e seja mais Pacífico!'",
     "Um homem entra em uma biblioteca e pede ao bibliotecário livros sobre paranóia. O bibliotecário sussurra: 'Eles estão bem atrás de você!'",
     "Por que os coalas não contam como ursos? Eles não têm as coalificações certas.",
     "Algumas pessoas comem caracóis. Eles não devem gostar de fast food.",
     "Como você chama uma pilha de gatinhos? Um meowntanha.",
-    "O que fica mais úmido quanto mais seca? Uma toalha.",
-    "Eu tenho uma piada sobre viagem no tempo, mas não vou contar. Vocês não gostaram.",
-    "Alguém conhece uma boa piada sobre o sódio? R: Na…",
     "Como você chama o monstro mais inteligente de todos? FrankEinstein.",
-    "Você ouviu sobre todos os significados ocultos do Rei Leão? Sim, está cheio de simbalismo."
+    "O que a parede disse para o teto? “Eu te cubro!",
+    "Por que os programadores não gostam da natureza? Muitos bugs.",
+    "Qual é o animal favorito do computador? O mouse.",
+    "Qual é a banda favorita dos físicos? AC/DC.",
+    "O que o neurônio disse ao outro neurônio? “Você tá pensando o que eu tô pensando”",
+    "Por que o computador foi ao médico? Porque ele tinha um vírus!",
+    "Como o banco de dados se declarou para a tabela? “Você é a chave primária do meu coração.”",
+    "Por que o químico não conseguia resolver o enigma? Ele não tinha a solução.",
+    "Qual é o café favorito do desenvolvedor? Java."
 ];
 
-const aleatoryNumberFunction = (min : number, max : number) => {
-  return Math.floor(Math.random() * (max - min - 1) - min)
-}
-
-const setJokes = () => {
-  if (jokes.length > 0){
-    const aleatoryNumber = aleatoryNumberFunction(0, jokes.length - 1)
-    const joke = jokes[aleatoryNumber]
-    jokes.splice(aleatoryNumber, 1)
-    return joke
-  } else {
-    window.alert("ARRAY VAZIO")
-  }
-}
 export async function GET() {
-  const joke = setJokes()
-    return new Response(JSON.stringify(joke), {
+    return new Response(JSON.stringify(jokes), {
         status: 200,
         headers: {
           'Content-Type': 'application/json',
--- src/app/components/Jokes.tsx
+++ src/app/components/Jokes.tsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 export default function Jokes() {
   const [jokes, setJokes] = useState<string[]>([]); 
   const [loading, setLoading] = useState<boolean>(true); 
+  const [currentJoke, SetCurrentJoke] = useState(""); 
 
   useEffect(() => {
     async function fetchJokes() {
@@ -13,27 +14,41 @@ export default function Jokes() {
         if (!response.ok) {
           throw new Error(`Response status: ${response.status}`);
         }
-        setLoading(false)
         const json = await response.json();
-        console.log(json);
         setJokes(json)
+        setLoading(false)
       } catch (error) {
         console.error(error);
+        setLoading(false)
       }
     }
    
     fetchJokes();
   }, []);
 
-  if (loading) return <p>Carregando piadas...</p>;
+  const aleatoryNumberFunction = (min : number, max : number) => {
+    return Math.floor(Math.random() * (max - min - 1) + min)
+  }
 
+  const jokesfunction = () => {
+    if (jokes.length > 0) {
+      if (loading){
+        return <p>Carregando piadas...</p>;
+      }
+      const aleatoryNumber = aleatoryNumberFunction(0, jokes.length - 1)
+      SetCurrentJoke(jokes[aleatoryNumber])
+      jokes.splice(aleatoryNumber, 1)
+    } else {
+      window.alert("ARRAY VAZIO")
+    }
+  }
 
   return (
     <div>
       <h1>Piadas</h1>
-      <button >clique</button>
+      <button onClick={jokesfunction}>clique</button>
       <p>
-        {jokes}
+        {currentJoke}
       </p>
     </div>
   );
