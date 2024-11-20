3b41e2b853e9d82a7e77a389f7d229213d16cd8a
christian <christiandeveloper123@gmail.com>
2024-11-20 16:56:57 -0300

    implementação da lógica das cartas viradas

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,29 +1,47 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
-import { GiCardAceClubs } from "react-icons/gi";
-
+import Image from 'next/image';
+import BlueCard from "../../../public/BlueCaard.png";
+import RedCard from "../../../public/RedCard.png";
 interface JokeProps {
   joke: string;
 }
 
 export default function Card({ joke }: JokeProps) {
-  const [flipped, setFlipped] = useState(false);
+  const [flippedRedCard, setFlippedRedCard] = useState(false);
+  const [flippedBlueCard, setFlippedBlueCard] = useState(false);
 
   return (
-    <div>
+    <div className='flex gap-10 items-center justify-center'>
       <motion.div
+        className={`w-[300px] shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedBlueCard ? 'bg-[#370A77]' : ''}`} 
         initial={{ rotateY: 0 }}
-        animate={{ rotateY: flipped ? 180 : 0 }}
+        animate={{ rotateY: flippedBlueCard ? 180 : 0 }}
         transition={{ duration: 0.6 }}
-        onClick={() => setFlipped(!flipped)}
-        >
-        <GiCardAceClubs className="text-[600px] cursor-pointer " />
+        onClick={() => setFlippedBlueCard(!flippedBlueCard)}
+      >
+        {flippedBlueCard ? (
+            ""
+        ):( 
+          <Image className='' src={BlueCard} alt='Carta azul com a piada' width={300} height={300} />
+        )}
       </motion.div>
-      {flipped && (
-        <div>
-          {joke}
-        </div>
-      )}
+      <motion.div
+        className={`w-[300px] shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedRedCard ? 'bg-[#F55F56]' : ''}`} 
+        initial={{ rotateY: 0 }}
+        animate={{ rotateY: flippedRedCard ? 180 : 0 }}
+        transition={{ duration: 0.6 }}
+        onClick={() => setFlippedRedCard(!flippedRedCard)}
+      >
+        {flippedRedCard ? (
+            ""
+        ):( 
+          <Image className='' src={RedCard} alt='Carta azul com a piada' width={300} height={300} />
+        )}
+      </motion.div>
+      <div className="mt-4 text-center">
+        {joke}
+      </div>
     </div>
   );
 }
