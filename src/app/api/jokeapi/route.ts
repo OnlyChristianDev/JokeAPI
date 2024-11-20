a94c9073c6511fb6a223eabef9e239feefd72ca4
christian <christiandeveloper123@gmail.com>
2024-11-20 17:01:34 -0300

    transformação na estrutura da API

--- src/app/api/jokeapi/route.ts
+++ src/app/api/jokeapi/route.ts
@@ -1,34 +1,124 @@
 const jokes = [
-    "Por que os químicos são ótimos em resolver problemas? Porque eles têm todas as soluções!",
-    "Por que o desenvolvedor faliu? Porque ele usou todo o seu cache.",
-    "Você já ouviu falar do cara que roubou o calendário? Ele pegou 12 meses!",
-    "Meus professores me disseram que eu nunca seria muito porque procrastino muito. Eu disse a eles: 'Esperem para ver!'",
-    "Quando em uma candidatura a emprego perguntam quem deve ser notificado em caso de emergência, sempre escrevo: 'Um médico muito bom'.",
-    "Por que o médico está sempre calmo? Porque ele tem muitos pacientes.",
-    "Por que o livro de matemática parece tão triste? Por causa de todos os seus problemas.",
-    "Como chamar um cão mágico? Um Labracadabrador.",
-    "Por que os pássaros voam para climas mais quentes no inverno? É muito mais fácil do que caminhar!",
-    "Seja como um próton. Sempre seja positivo.",
-    "Que tipo de carro o Yoda dirige? Toyoda.", 
-    "O que o zero diz a um oito? Cinto legal.",
-    "O que o HTML disse ao CSS? “Eu gosto do seu estilo!.",
-    "O que você faz quando suas piadas sobre ciência não tem graça? Continue tentando até conseguir uma reação.",
-    "Por que o dinossauro atravessou a rua? Porque as galinhas ainda não existiam!",
-    "O que o Oceano Atlântico disse ao Oceano Índico? 'Experimente e seja mais Pacífico!'",
-    "Um homem entra em uma biblioteca e pede ao bibliotecário livros sobre paranóia. O bibliotecário sussurra: 'Eles estão bem atrás de você!'",
-    "Por que os coalas não contam como ursos? Eles não têm as coalificações certas.",
-    "Algumas pessoas comem caracóis. Eles não devem gostar de fast food.",
-    "Como você chama uma pilha de gatinhos? Um meowntanha.",
-    "Como você chama o monstro mais inteligente de todos? FrankEinstein.",
-    "O que a parede disse para o teto? “Eu te cubro!",
-    "Por que os programadores não gostam da natureza? Muitos bugs.",
-    "Qual é o animal favorito do computador? O mouse.",
-    "Qual é a banda favorita dos físicos? AC/DC.",
-    "O que o neurônio disse ao outro neurônio? “Você tá pensando o que eu tô pensando”",
-    "Por que o computador foi ao médico? Porque ele tinha um vírus!",
-    "Como o banco de dados se declarou para a tabela? “Você é a chave primária do meu coração.”",
-    "Por que o químico não conseguia resolver o enigma? Ele não tinha a solução.",
-    "Qual é o café favorito do desenvolvedor? Java."
+  {
+      joke: "Por que os químicos são ótimos em resolver problemas?",
+      answer: "Porque eles têm todas as soluções!"
+  },
+  {
+      joke: "Por que o desenvolvedor faliu?",
+      answer: "Porque ele usou todo o seu cache."
+  },
+  {
+      joke: "Você já ouviu falar do cara que roubou o calendário?",
+      answer: "Ele pegou 12 meses!"
+  },
+  {
+      joke: "Meus professores me disseram que eu nunca seria muito porque procrastino muito.",
+      answer: "Eu disse a eles: 'Esperem para ver!'"
+  },
+  {
+      joke: "Quando em uma candidatura a emprego perguntam quem deve ser notificado em caso de emergência, sempre escrevo:",
+      answer: "'Um médico muito bom.'"
+  },
+  {
+      joke: "Por que o médico está sempre calmo?",
+      answer: "Porque ele tem muitos pacientes."
+  },
+  {
+      joke: "Por que o livro de matemática parece tão triste?",
+      answer: "Por causa de todos os seus problemas."
+  },
+  {
+      joke: "Como chamar um cão mágico?",
+      answer: "Um Labracadabrador."
+  },
+  {
+      joke: "Por que os pássaros voam para climas mais quentes no inverno?",
+      answer: "É muito mais fácil do que caminhar!"
+  },
+  {
+      joke: "Seja como um próton.",
+      answer: "Sempre seja positivo."
+  },
+  {
+      joke: "Que tipo de carro o Yoda dirige?",
+      answer: "Toyoda."
+  },
+  {
+      joke: "O que o zero diz a um oito?",
+      answer: "Cinto legal."
+  },
+  {
+      joke: "O que o HTML disse ao CSS?",
+      answer: "“Eu gosto do seu estilo!”"
+  },
+  {
+      joke: "O que você faz quando suas piadas sobre ciência não têm graça?",
+      answer: "Continue tentando até conseguir uma reação."
+  },
+  {
+      joke: "Por que o dinossauro atravessou a rua?",
+      answer: "Porque as galinhas ainda não existiam!"
+  },
+  {
+      joke: "O que o Oceano Atlântico disse ao Oceano Índico?",
+      answer: "'Experimente e seja mais Pacífico!'"
+  },
+  {
+      joke: "Um homem entra em uma biblioteca e pede ao bibliotecário livros sobre paranóia.",
+      answer: "O bibliotecário sussurra: 'Eles estão bem atrás de você!'"
+  },
+  {
+      joke: "Por que os coalas não contam como ursos?",
+      answer: "Eles não têm as coalificações certas."
+  },
+  {
+      joke: "Algumas pessoas comem caracóis.",
+      answer: "Eles não devem gostar de fast food."
+  },
+  {
+      joke: "Como você chama uma pilha de gatinhos?",
+      answer: "Um meowntanha."
+  },
+  {
+      joke: "Como você chama o monstro mais inteligente de todos?",
+      answer: "FrankEinstein."
+  },
+  {
+      joke: "O que a parede disse para o teto?",
+      answer: "“Eu te cubro!”"
+  },
+  {
+      joke: "Por que os programadores não gostam da natureza?",
+      answer: "Muitos bugs."
+  },
+  {
+      joke: "Qual é o animal favorito do computador?",
+      answer: "O mouse."
+  },
+  {
+      joke: "Qual é a banda favorita dos físicos?",
+      answer: "AC/DC."
+  },
+  {
+      joke: "O que o neurônio disse ao outro neurônio?",
+      answer: "“Você tá pensando o que eu tô pensando?”"
+  },
+  {
+      joke: "Por que o computador foi ao médico?",
+      answer: "Porque ele tinha um vírus!"
+  },
+  {
+      joke: "Como o banco de dados se declarou para a tabela?",
+      answer: "“Você é a chave primária do meu coração.”"
+  },
+  {
+      joke: "Por que o químico não conseguia resolver o enigma?",
+      answer: "Ele não tinha a solução."
+  },
+  {
+      joke: "Qual é o café favorito do desenvolvedor?",
+      answer: "Java."
+  }
 ];
 
 export async function GET() {
--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -12,9 +12,9 @@ export default function Card({ joke }: JokeProps) {
   const [flippedBlueCard, setFlippedBlueCard] = useState(false);
 
   return (
-    <div className='flex gap-10 items-center justify-center'>
+    <div className='flex  gap-10 items-center justify-center'>
       <motion.div
-        className={`w-[300px] shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedBlueCard ? 'bg-[#370A77]' : ''}`} 
+        className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedBlueCard ? 'bg-[#370A77]' : ''}`} 
         initial={{ rotateY: 0 }}
         animate={{ rotateY: flippedBlueCard ? 180 : 0 }}
         transition={{ duration: 0.6 }}
@@ -27,21 +27,20 @@ export default function Card({ joke }: JokeProps) {
         )}
       </motion.div>
       <motion.div
-        className={`w-[300px] shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedRedCard ? 'bg-[#F55F56]' : ''}`} 
+        className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedRedCard ? 'bg-[#F55F56]' : ''}`} 
         initial={{ rotateY: 0 }}
         animate={{ rotateY: flippedRedCard ? 180 : 0 }}
         transition={{ duration: 0.6 }}
         onClick={() => setFlippedRedCard(!flippedRedCard)}
       >
+         {joke}
         {flippedRedCard ? (
             ""
         ):( 
           <Image className='' src={RedCard} alt='Carta azul com a piada' width={300} height={300} />
         )}
       </motion.div>
-      <div className="mt-4 text-center">
-        {joke}
-      </div>
+
     </div>
   );
 }
