b26542f488f32457a3da11750b10bc3f27fb9715
christian <christiandeveloper123@gmail.com>
2024-11-21 18:33:59 -0300

    Adição de instruções para melhorar o UI/UX

---
+++ src/app/components/Aviso.tsx
@@ -0,0 +1,9 @@
+export default function Aviso(){
+    return(
+        <div className="bg-white h-12 p-5 absolute bottom-24 rounded-xl flex items-center justify-center">
+            <h1 className="text-black text-center text-md font-semibold">
+                Ei, descubra a piada antes! Vire a carta azul primeiro.
+            </h1>
+        </div>
+    )
+}
--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 import Image from 'next/image';
 import BlueCard from "../../../public/BlueCaard.png";
 import RedCard from "../../../public/RedCard.png";
+import Aviso from './Aviso';
 interface JokeProps {
   joke: string;
   answer : string;
@@ -32,55 +33,54 @@ export default function Card({ joke, answer, onNextJoke  }: JokeProps) {
       setFlippedBlueCard(true)
       onNextJoke()
     }
-
-    onNextJoke()
   }
 
   useEffect(()=> {
     if (animation) {
-      
-      setTimeout(() => setAnimation(false), 600); 
+      setTimeout(() => setAnimation(false), 1600); 
     }
-   
   },[animation])
 
   return (
-    <div className='flex gap-10 items-center justify-center'>
-      <motion.div
-        className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedBlueCard ? 'bg-[#370A77] p-10' : ''}`} 
-        initial={{ rotateY: 0 }}
-        animate={{
-          rotateY: flippedBlueCard ? 180 : 0,
-          x: animation ? [0, 10, -10, 10, -10, 0] : 0, 
-        }}
-        transition={{ duration: 0.6 }}
-        onClick={flippedRedCardFunction}
-      >
-        {flippedBlueCard ?  <p className='font-semibold scale-x-[-1] text-center text-2xl'>
-        {joke}
-            </p> : ""}
-        {flippedBlueCard ? (
-            ""
-        ):( 
-          <Image className='' src={BlueCard} alt='Carta azul com a piada' width={300} height={300} />
-        )}
-      </motion.div>
-      <motion.div
-        className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedRedCard ? 'bg-[#F55F56] p-10' : ''}`} 
-        initial={{ rotateY: 0 }}
-        animate={{ rotateY: flippedRedCard ? 180 : 0 }}
-        transition={{ duration: 0.6 }}
-        onClick={flippedBlueCardFunction}
-      >
-        {flippedRedCard ?  <p className='text-2xl scale-x-[-1] text-center font-semibold'>
-        {answer}
-            </p> : ""}
-        {flippedRedCard ? (
-            ""
-        ):( 
-          <Image className='' src={RedCard} alt='Carta azul com a piada' width={300} height={300} />
-        )}
-      </motion.div>
+    <div className='flex flex-col items-center justify-center'>
+      <div className='flex gap-10'>
+        <motion.div
+          className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedBlueCard ? 'bg-[#370A77] p-10' : ''}`} 
+          initial={{ rotateY: 0 }}
+          animate={{
+            rotateY: flippedBlueCard ? 180 : 0,
+            x: animation ? [0, 10, -10, 10, -10, 0] : 0, 
+          }}
+          transition={{ duration: 0.6 }}
+          onClick={flippedRedCardFunction}
+        >
+          {flippedBlueCard ?  <p className='font-semibold scale-x-[-1] text-center text-2xl'>
+          {joke}
+              </p> : ""}
+          {flippedBlueCard ? (
+              ""
+          ):( 
+            <Image className='' src={BlueCard} alt='Carta azul com a piada' width={300} height={300} />
+          )}
+        </motion.div>
+        <motion.div
+          className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${flippedRedCard ? 'bg-[#F55F56] p-10' : ''}`} 
+          initial={{ rotateY: 0 }}
+          animate={{ rotateY: flippedRedCard ? 180 : 0 }}
+          transition={{ duration: 0.6 }}
+          onClick={flippedBlueCardFunction}
+        >
+          {flippedRedCard ?  <p className='text-2xl scale-x-[-1] text-center font-semibold'>
+          {answer}
+              </p> : ""}
+          {flippedRedCard ? (
+              ""
+          ):( 
+            <Image className='' src={RedCard} alt='Carta azul com a piada' width={300} height={300} />
+          )}
+        </motion.div>
+      </div>
+      {animation == true ? (<Aviso />) : ""}
     </div>
   );
 }
