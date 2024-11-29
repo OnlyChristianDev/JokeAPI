4a9e6c78de916ff89b2ab3ff6613eca1d7a5e07f
christian <christiandeveloper123@gmail.com>
2024-11-29 16:35:13 -0300

    Media query

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -65,59 +65,60 @@ export default function Card({ joke, answer, onNextJoke, animationArray, ArrayNu
  
   return (
     <div className="flex flex-col items-center justify-center">
-      <div className="flex gap-10">
+      <div className="flex max-sm:flex-col gap-10">
         <div className="flex items-center justify-center flex-col gap-5">
         <AnimatePresence>
           {showIndenticacao && <Indenticacao nomeDaCarta={"Piadas"} />} 
           </AnimatePresence>
           <motion.div
-            className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${
-              flippedBlueCard ? "bg-[#9A6AF9] p-10" : ""
-            }`}
-            initial={{ rotateY: 0 }}
-            animate={{
-              rotateY: flippedBlueCard ? 180 : 0,
-              x: animation ? [0, 10, -10, 10, -10, 0] : 0,
-            }}
-            transition={{ duration: 0.6 }}
-            onClick={flippedRedCardFunction}
-          >
-            {flippedBlueCard ? (
-              <p className="font-semibold scale-x-[-1] text-center text-2xl">{joke}</p>
-            ) : (
-              <Image
-                className=""
-                src={BlueCard}
-                alt="Carta azul com a piada"
-                width={300}
-                height={300}
-              />
-            )}
-          </motion.div>
+  className={`w-[300px] h-[412px] cursor-pointer shadow-2xl rounded-lg flex items-center justify-center transition-colors duration-600 ${
+    flippedBlueCard ? "bg-[#9A6AF9] p-10" : ""
+  } 
+  max-sm:w-[220px] max-sm:h-[310px]`}
+  initial={{ rotateY: 0 }}
+  animate={{
+    rotateY: flippedBlueCard ? 180 : 0,
+    x: animation ? [0, 10, -10, 10, -10, 0] : 0,
+  }}
+  transition={{ duration: 0.6 }}
+  onClick={flippedRedCardFunction}
+>
+  {flippedBlueCard ? (
+    <p className="font-semibold scale-x-[-1] text-center text-2xl">{joke}</p>
+  ) : (
+    <Image
+      className="w-full h-full object-contain rounded-lg"
+      src={BlueCard}
+      alt="Carta azul com a piada"
+    />
+  )}
+</motion.div>
         </div>
         <div className="flex items-center justify-center flex-col gap-5">
           {showIndenticacao && <Indenticacao nomeDaCarta={"Respostas"} />}
           <motion.div
-            className={`w-[300px] cursor-pointer shadow-2xl h-[412px] rounded-lg flex items-center justify-center transition-colors duration-600 ${
-              flippedRedCard ? "bg-[#F55F56] p-10" : ""
-            }`}
-            initial={{ rotateY: 0 }}
-            animate={{ rotateY: flippedRedCard ? 180 : 0 }}
-            transition={{ duration: 0.6 }}
-            onClick={flippedBlueCardFunction}
-          >
-            {flippedRedCard ? (
-              <p className="text-2xl scale-x-[-1] text-center font-semibold">{answer}</p>
-            ) : (
-              <Image
-                className=""
-                src={RedCard}
-                alt="Carta azul com a piada"
-                width={300}
-                height={300}
-              />
-            )}
-          </motion.div>
+  className={`w-[300px] h-[412px] cursor-pointer shadow-2xl rounded-lg flex items-center justify-center transition-colors duration-600 ${
+    flippedBlueCard ? "bg-[#F55F56] p-10" : ""
+  } 
+  max-sm:w-[220px] max-sm:h-[310px]`}
+  initial={{ rotateY: 0 }}
+  animate={{
+    rotateY: flippedBlueCard ? 180 : 0,
+    x: animation ? [0, 10, -10, 10, -10, 0] : 0,
+  }}
+  transition={{ duration: 0.6 }}
+  onClick={flippedRedCardFunction}
+>
+  {flippedBlueCard ? (
+    <p className="font-semibold scale-x-[-1] text-center text-2xl">{joke}</p>
+  ) : (
+    <Image
+      className="w-full h-full object-contain rounded-lg"
+      src={RedCard}
+      alt="Carta azul com a piada"
+    />
+  )}
+</motion.div>
         </div>
       </div>
       <AnimatePresence>
